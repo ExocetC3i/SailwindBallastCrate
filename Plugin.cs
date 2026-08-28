edd6213f2913c5204ae1efd26e872a1b204f18c3
ExocetC3i <christopher.l.greer@gmail.com>
2026-08-27 23:49:37 -0700

    Revised plugin code with ItemRigidbody

--- Plugin.cs
+++ Plugin.cs
@@ -6,15 +6,18 @@ using System.Globalization;
 
 // Developed with GPT-5.6 Luna
 
-namespace BallastCrateMod
+namespace BallastCrate
 {
     [BepInPlugin("com.Exocet.ballastcrate", "Ballast Crate Mod", "1.0.0")]
     public class BallastCratePlugin : BaseUnityPlugin
     {
         private const string ModDataPrefix = "BallastCrateMod.item.";
+        private const string MassSuffix = ".mass";
+        private const string NameSuffix = ".name";
+        private const string ValueSuffix = ".value";
         private const float BallastMass = 10000f;
         private const int BallastValue = 1;
-        private const string BallastName = "10 tonne Ballast Crate";
+        private const string BallastName = "10,000 Weight Ballast Crate";
 
         private void Awake()
         {
@@ -32,9 +35,7 @@ namespace BallastCrateMod
 
         private void SpawnBallastCrate()
         {
-            // Copying a lot of code from NANDCommand's item spawner script;
 
-            // gets a directory list of all prefabs in the
             var directory = SaveLoadManager.instance.GetComponent<PrefabsDirectory>();  
             
             GameObject ballast = directory.directory[23]; //copper ore crate
@@ -49,7 +50,7 @@ namespace BallastCrateMod
             item.sold = true;
             saveable.RegisterToSave();
             ApplyBallastProperties(item);
-            GameState.modData[GetModDataKey(saveable.instanceId)] = BallastMass.ToString(CultureInfo.InvariantCulture);
+            SaveBallastProperties(saveable.instanceId, item);
             obj.GetComponent<Good>().RegisterAsMissionless();
 
 
@@ -66,11 +67,31 @@ namespace BallastCrateMod
 
             foreach (var saveable in UnityEngine.Object.FindObjectsOfType<SaveablePrefab>())
             {
-                if (GameState.modData.ContainsKey(GetModDataKey(saveable.instanceId)))
+                if (IsBallastCrate(saveable.instanceId))
                 {
                     ApplyBallastProperties(saveable.GetComponent<ShipItem>());
                 }
             }
+        }
+
+        internal static void RestoreLoadedBallastCrate(ShipItem item)
+        {
+            if (item == null || item.GetComponent<SaveablePrefab>() == null)
+            {
+                return;
+            }
+
+            if (IsBallastCrate(item.GetComponent<SaveablePrefab>().instanceId))
+            {
+                ApplyBallastProperties(item);
+            }
+        }
+
+        private static bool IsBallastCrate(int instanceId)
+        {
+            return GameState.modData != null &&
+                (GameState.modData.ContainsKey(GetModDataKey(instanceId)) ||
+                 GameState.modData.ContainsKey(GetModDataKey(instanceId) + MassSuffix));
         }
 
         private static string GetModDataKey(int instanceId)
@@ -78,11 +99,46 @@ namespace BallastCrateMod
             return ModDataPrefix + instanceId;
         }
 
+        private static void SaveBallastProperties(int instanceId, ShipItem item)
+        {
+            string key = GetModDataKey(instanceId);
+            GameState.modData[key] = BallastMass.ToString(CultureInfo.InvariantCulture);
+            GameState.modData[key + MassSuffix] = item.mass.ToString(CultureInfo.InvariantCulture);
+            GameState.modData[key + NameSuffix] = item.name;
+            GameState.modData[key + ValueSuffix] = item.value.ToString(CultureInfo.InvariantCulture);
+        }
+
         private static void ApplyBallastProperties(ShipItem item)
         {
-            item.mass = BallastMass;
-            item.name = BallastName;
-            item.value = BallastValue;
+            if (item == null)
+            {
+                return;
+            }
+
+            SaveablePrefab saveable = item.GetComponent<SaveablePrefab>();
+            string key = GetModDataKey(saveable.instanceId);
+            float mass;
+            int value;
+
+            item.mass = GameState.modData != null &&
+                float.TryParse(GetModDataValue(key + MassSuffix, BallastMass.ToString(CultureInfo.InvariantCulture)),
+                    NumberStyles.Float, CultureInfo.InvariantCulture, out mass)
+                ? mass
+                : BallastMass;
+            item.name = GetModDataValue(key + NameSuffix, BallastName);
+            item.value = GameState.modData != null &&
+                int.TryParse(GetModDataValue(key + ValueSuffix, BallastValue.ToString(CultureInfo.InvariantCulture)),
+                    NumberStyles.Integer, CultureInfo.InvariantCulture, out value)
+                ? value
+                : BallastValue;
+        }
+
+        private static string GetModDataValue(string key, string fallback)
+        {
+            string value;
+            return GameState.modData != null && GameState.modData.TryGetValue(key, out value)
+                ? value
+                : fallback;
         }
     }
 
@@ -92,6 +148,24 @@ namespace BallastCrateMod
         private static void Postfix()
         {
             BallastCratePlugin.RestoreLoadedBallastCrates();
+        }
+    }
+
+    [HarmonyPatch(typeof(ShipItem), nameof(ShipItem.OnLoad))]
+    internal static class BallastCrateShipItemLoadPatch
+    {
+        private static void Postfix(ShipItem __instance)
+        {
+            BallastCratePlugin.RestoreLoadedBallastCrate(__instance);
+        }
+    }
+
+    [HarmonyPatch(typeof(ItemRigidbody), nameof(ItemRigidbody.UpdateMass))]
+    internal static class BallastCrateMassPatch
+    {
+        private static void Prefix(ItemRigidbody __instance)
+        {
+            BallastCratePlugin.RestoreLoadedBallastCrate(__instance.GetShipItem());
         }
     }
 }
